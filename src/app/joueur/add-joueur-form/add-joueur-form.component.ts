815c0896c7b36b2ca3751cc85d10f95e67bc09db
XiangLuc <xiangluc01@gmail.com>
2024-03-20 21:11:52 +0100

    élément test retiré

--- src/app/joueur/add-joueur-form/add-joueur-form.component.ts
+++ src/app/joueur/add-joueur-form/add-joueur-form.component.ts
@@ -12,24 +12,10 @@ import { FormsModule } from '@angular/forms';
 export class AddJoueurFormComponent implements OnInit {
 
   newJoueur: any;
-  private http = inject(HttpClient);
+
 
   ngOnInit(): void {
-    // Données JSON à envoyer
-    const data = { "_id": 1, "nom": "John Doe" };
-
-    // URL de l'endpoint à laquelle envoyer la requête POST
-    const url = 'http://localhost:5000/joueurs';
-
-    // Effectuer la requête POST
-    this.http.post(url, data).subscribe(
-      (response) => {
-        console.log('Réponse de la requête POST :', response);
-      },
-      (error) => {
-        console.error('Erreur lors de la requête POST :', error);
-      }
-    );
+   
   }
 
 }
