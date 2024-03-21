7d68cd4f04c7337df831e383219519874403c593
XiangLuc <xiangluc01@gmail.com>
2024-03-21 21:33:25 +0100

    ajout du composant détails joueur

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { JoueurListComponent } from './joueur/joueur-list/joueur-list.component';
 import { AddJoueurFormComponent } from './joueur/add-joueur-form/add-joueur-form.component';
+import { DetailsJoueurComponent } from './joueur/details-joueur/details-joueur.component';
 
 export const routes: Routes = [
     { path: 'liste_joueurs', component: JoueurListComponent },
-    { path: 'form-joueur', component: AddJoueurFormComponent}
+    { path: 'form-joueur', component: AddJoueurFormComponent},
+    { path: 'details/:id', component: DetailsJoueurComponent}
 ];
---
+++ src/app/joueur/details-joueur/details-joueur.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-joueur',
+  standalone: true,
+  imports: [],
+  templateUrl: './details-joueur.component.html',
+  styleUrl: './details-joueur.component.css'
+})
+export class DetailsJoueurComponent {
+
+}
