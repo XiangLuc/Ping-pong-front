7ffbb8c8a8f0532c6171259c1e2151408e53f6ed
XiangLuc <xiangluc01@gmail.com>
2024-03-21 21:56:40 +0100

    formulaire temporaire avec requete post fonctionelle

--- src/app/joueur/add-joueur-form/add-joueur-form.component.ts
+++ src/app/joueur/add-joueur-form/add-joueur-form.component.ts
@@ -1,19 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { JoueurService } from '../joueur.service';
 
 @Component({
   selector: 'app-add-joueur-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-joueur-form.component.html',
   styleUrl: './add-joueur-form.component.css'
 })
 export class AddJoueurFormComponent implements OnInit {
 
-  newJoueur: any;
+  newJoueur: any = {
+    categorie: { age: null, niveau: null }
+  };
 
+  constructor(private joueurService: JoueurService) {}
 
-  ngOnInit(): void {
-   
+  ngOnInit(): void {}
+
+  addJoueur() {
+
+    this.joueurService.addJoueur(this.newJoueur).subscribe(
+      (response) => {
+        console.log('Joueur ajouté avec succès:', response);
+        this.newJoueur = {
+          categorie: { age: null, niveau: null }
+        };
+      }
+    );
+  }
+
+  isFormValid() {
+    return this.newJoueur.nom && this.newJoueur.prenom && this.newJoueur.categorie?.age && this.newJoueur.categorie?.niveau && this.newJoueur.sexe;
   }
 
 }
