64d045e0bc919023464edc5146c1df761a6958c3
XiangLuc <xiangluc01@gmail.com>
2024-03-26 10:14:10 +0100

    suppression de la génération de _id aléatoire

--- src/app/joueur/add-joueur-form/add-joueur-form.component.ts
+++ src/app/joueur/add-joueur-form/add-joueur-form.component.ts
@@ -15,7 +15,6 @@ export class AddJoueurFormComponent implements OnInit {
   message:any = undefined;
 
   newJoueur: any = {
-    _id: Math.random().toString(36).substr(2, 9),
     categorie: { age: null, niveau: null },
     point: 0 
   };
@@ -31,7 +30,6 @@ export class AddJoueurFormComponent implements OnInit {
         this.message = response;
         console.log('Joueur ajouté avec succès:', response);
         this.newJoueur = {
-          _id: Math.random().toString(36).substr(2, 9),
           categorie: { age: null, niveau: null },
           point: 0
         };
